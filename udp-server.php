d363ece1b12c6d6bac41866e3ca09c8f97467a26
Emmanuel Noguera <nogueraruiz@gmail.com>
2018-11-14 13:17:20 -0300

    server udp

--- udp-server.php
+++ udp-server.php
@@ -2,37 +2,48 @@
 if (!extension_loaded('sockets')) {
     die('The sockets extension is not loaded.');
 }
-// create unix udp socket
+
+// conf socket
 $host = '127.0.0.1';
-$port = 4568;
+$port = 7777;
+
+// create unix udp socket
 $socket = socket_create(AF_INET, SOCK_DGRAM, SOL_UDP);
-if (!$socket)
-        die('Unable to create AF_UNIX socket');
-
-
-// Bind, listen and disable blocking
-if ($socket === FALSE || ! socket_bind($socket, $host, $port)) trigger_socket_error();
-
-
-$clients = []; 
-while (true){ 
-    socket_recvfrom($socket, $buffer, 32768, 0, $ip, $port) === true or onSocketFailure("Failed to receive packet", $socket); 
-    $address = "$ip:$port"; 
-    if (!isset($clients[$address])) $clients[$address] = new Client(); 
-    $clients[$address]->handlePacket($buffer); 
+if (!$socket) {
+    onSocketFailure("Failed to create socket", $socket);
 }
 
+// reuseable port
+socket_set_option($socket, SOL_SOCKET, SO_REUSEADDR, 1);
 
-  /**
-   * Trigger an exception with the last socket error.
-   *
-   * @param Socket
-   */
-  function trigger_socket_error($socket = null)
-  {
-    $code = $socket ? socket_last_error($socket) : socket_last_error();
-    debug_print_backtrace();
-    trigger_error("[${code}] " . socket_strerror($code), E_USER_ERROR);
-  }
-  
-?>
+// bind
+if ($socket === false || !socket_bind($socket, $host, $port)) {
+    socket_close($socket);
+    onSocketFailure("Failed to bind socket", $socket);
+}
+
+$clients = [];
+while (true) {
+    socket_recvfrom($socket, $buffer, 65535, 0, $clientIP,$clientPort);
+    $address = "$clientIP:$clientPort";
+    //if (!isset($clients[$address])) {
+    //    $clients[$address] = new Client();
+    //}
+
+    //$clients[$address]->handlePacket($buffer);
+    echo "Received $buffer from remote address $clientIP and remote port $clientPort" . PHP_EOL;
+}
+
+/**
+ * Trigger an exception with the last socket error.
+ *
+ * @param String
+ * @param Socket
+ */
+function onSocketFailure(string $message, $socket = null)
+{
+    if (is_resource($socket)) {
+        $message .= ": " . socket_strerror(socket_last_error($socket));
+    }
+    die($message);
+}
